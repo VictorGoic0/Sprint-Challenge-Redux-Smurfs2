60def0f5f4096d2f8f2e87b36888e2d38578a942
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-08 12:07:15 -0500

    React router added as dependency. Smurfs and Smurf component built out.

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './Smurf.css';
+
+class Smurf extends Component {
+  render() {
+    return (
+      <h2>placeholder</h2>
+      /*{<div className="Smurf">
+
+      <header>
+        <h2>{props.smurf.name}</h2>
+        <button onClick={e => this.deleteSmurf(e, props.id)}>X</button>
+      </header>
+
+      <div className="height">
+        <h3>height:</h3>
+        <p>{props.smurf.height} tall</p>
+      </div>
+
+      <div className="age">
+        <h3>Age:</h3>
+        <p>{props.smurf.age} smurf years old</p>
+      </div>
+
+      <div className="edit">
+        <Link to={{ pathname: "/edit-form", state: {
+          name: props.name,
+          age: props.age,
+          height: props.height,
+          id: props.id
+        } }}>
+          <button>Edit Smurf</button>
+        </Link>
+      </div>
+    </div>}*/
+    )
+  }
+}
+
+export default Smurf;
---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Smurf from './Smurf';
+
+const Smurfs = props => {
+  return (
+    //{props.smurfs.map(smurf => <Smurf key={smurf.id} smurf={smurf} />)}
+      <h1>placeholder</h1>
+  )
+}
+
+export default Smurfs;
