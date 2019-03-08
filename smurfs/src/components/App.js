908aca0b281af409aec4e5f9c862898068e15759
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-08 12:15:18 -0500

    Navigation added, rendered in App. Routes added to app, App wrapped in Router. CSS reset added to index. SmurfForm added and styled.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Smurfs from './Smurfs';
+import Navigation from './Navigation';
+import SmurfForm from './SmurfForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Smurfs />
-      </div>
+      <Router>
+        <div className="App">
+          <Navigation />
+          <Route path="/" component={Smurfs} />
+          <Route path="/smurf-form" component={SmurfForm} />
+        </div>
+      </Router>
     );
   }
 }
